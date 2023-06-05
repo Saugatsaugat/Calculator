bf1a5e088d98e6cf1b0889cfdb9379d5f02954f8
Saugatsaugat <saugatthapa2073@gmail.com>
2023-06-05 14:45:26 +0545

    Initial Commit

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author saugat
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+    }
+}
